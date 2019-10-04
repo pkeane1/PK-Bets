1289f2ac9867128918c5ebe51639f5b0721a7187
Pierce Keane <Pierce.Keane@gmail.com>
2019-10-04 19:01:18 -0400

    components and pages set up along with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+   <Router>
+       <Route exact path="/" component={Home} />
+   </Router>
   );
 }
 
