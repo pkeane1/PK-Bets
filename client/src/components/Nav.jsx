1f23e95a7053ae3163bc3fab876e45258adfd14d
Pierce Keane <Pierce.Keane@gmail.com>
2019-10-08 18:56:16 -0400

    cant decide what i want for UI

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,13 +3,22 @@ import React from "react";
 
 
 const Nav = () => {
-    return(
+    return (
         <nav className='navbar sticky-top navbar-expand-lg navbar-light'>
-          <h1>
-            <strong>PK Bets</strong>
-          </h1>
-    
-        <ul className='nav ml-auto' id='nav'></ul>
+            <h1>
+                <strong>PK Bets</strong>
+            </h1>
+
+            <ul className='nav ml-auto' id='nav'>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">login</a>
+                </li>
+
+            </ul>
+
         </nav>
     )
 }
